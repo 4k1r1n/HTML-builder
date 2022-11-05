a7df81d09543e9021ad5a5cf1109a37d95bf2bc3
Arina <akira657@mail.ru>
2022-11-06 01:28:34 +0300

    feat: add script 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const dirPath = path.join(__dirname, 'secret-folder');
+
+(async () => {
+    try {
+        const files = await fs.promises.readdir(dirPath, { withFileTypes: true });
+        files.forEach(async file => {
+            if (file.isFile()) {
+                const stats = await fs.promises.stat(path.join(dirPath, file.name));
+                const fileName = file.name.split('.')[0];
+                const fileType = path.extname(file.name).slice(1);
+                const fileSize = stats.size;
+                console.log(`${fileName} - ${fileType} - ${fileSize}bytes`);
+            }
+        })
+    } catch (err) {
+        console.error(err);
+    }
+})()
