3a6ce6711777e5377564f7798808bea53eef2910
Arina <akira657@mail.ru>
2022-11-06 18:59:32 +0300

    feat: add script 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 
+(async function copyDir() {
+    const dirPath = path.join(__dirname, 'files');
+    const copyDirPath = path.join(__dirname, 'files-copy');
+
+    try {
+        const files = await fs.promises.readdir(dirPath, { withFileTypes: true });
+        await fs.promises.rm(copyDirPath, { recursive: true, force: true });
+        await fs.promises.mkdir(copyDirPath, { recursive: true });
+        files.forEach(async file => {
+            if (file.isFile()) {
+                await fs.promises.copyFile(path.join(dirPath, file.name), path.join(copyDirPath, file.name));
+            }
+        })
+    } catch (err) {
+        console.error(err);
+    }
+})()
